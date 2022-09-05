058f0d33e7dd4665e22f59b85fd02b21b13143b3
Polaris-control <74716458+Polaris-control@users.noreply.github.com>
2022-09-05 14:49:51 +0900

    Update test2.cpp

--- HU_cpp_file/test2.cpp
+++ HU_cpp_file/test2.cpp
@@ -1,36 +1,38 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
-void _print(int* arr, int size) {
-	for (int i = 0; i < size;i++) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-}
-
 int main() {
-	int* len = new int[20];
-	for (int i = 0; i < 20; i++) {
-		len[i] = 0;
-	}
-	int len_size = 0;
+	int len[41], i =0;
+	int len_size =0;
 	while (1) {
 		int i = 0;
 		cin >> i;
-		if (!i) break;
 		len[len_size++] = i;
+		if (!i) break;
 	}
 	int height = 0;
+	int wide =0;
 	for (int i = 1; i < len_size; i += 2) {
 		height += len[i];
 	}
-	int k = 0, x = 0, y = 0;
-	int *results = new int[15];
-	while (cin >> x >> y) {
+	for(int i=0;i<len_size;i+=2){
+		wide += len[i];
+	}
+	int k = 0;
+	int x, y;
+	int results[15];
+	for (int i = 0; i< 15;i++) {
+		results[i] = 0;
+	}
+
+	while (k<15 && cin>>x>>y) {
+		if (x>wide && x<0){
+			results[k++] = -1;continue;
+		}
 		int result = 0;
 		int xptr = 0, yptr = 1, h = height, x_left = 0, x_right = len[xptr] + x_left;
-		for (int i = 0; i < len_size; i += 2) {
+		while (xptr < len_size) {
 			if (x_left < x && x < x_right) {
 				if (y < h) {
 					result = 1;
@@ -38,6 +40,7 @@ int main() {
 				else if (y > h) {
 					result = -1;
 				}
+				break;
 			}
 			else {
 				if (x == x_right) {
@@ -47,18 +50,18 @@ int main() {
 					else if (y < h - len[yptr]) {
 						result = 1;
 					}
+					break;
 				}
 				x_left = x_right;
-				x_right += len[xptr += 2];
+				xptr += 2;
+				x_right += len[xptr];
 				h -= len[yptr];
 				yptr += 2;
 			}
 		}
 		results[k++] = result;
-		
 	}
-
-	for (int i = 0;i < k;i++) {
+	for(int i=0;i<k;i++){
 		if (results[i] == 0) {
 			cout << "on" << endl;
 		}
@@ -69,6 +72,6 @@ int main() {
 			cout << "in" << endl;
 		}
 	}
-	
+
 	return 0;
 }
